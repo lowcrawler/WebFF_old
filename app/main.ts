b661e07b1ea52cb26aad158864ed90e9cd0c0a38
Federer <jfederer@usgs.gov>
2016-05-17 14:23:33 -0500

    Added router to bootstrap, templateURL to webff, and some basic stylesheet work

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import { bootstrap }    from '@angular/platform-browser-dynamic';
+import { ROUTER_PROVIDERS } from '@angular/router';
 
 import { WebFieldForm } from './webff.component';
 
-bootstrap(WebFieldForm);
+bootstrap(WebFieldForm, [ROUTER_PROVIDERS]);
