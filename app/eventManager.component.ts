978a40b1939d42b746dfd2394d6c7614e51951b3
Federer <jfederer@usgs.gov>
2016-05-17 16:44:08 -0500

    Removed MM, put links directly in WebFF template, got routing working

---
+++ app/eventManager.component.ts
@@ -0,0 +1,21 @@
+import { Component }       	from '@angular/core';
+//import { EventService }		from 'app/sedwe/event.service';
+
+@Component ({
+	selector: 
+		'eventMgr',
+	templateUrl: 
+		'./app/eventManager.component.html',
+//	providers: [EventService],
+})
+
+
+export class EventManagerComponent  { //} implements OnInit{
+//	constructor(private eventService: EventService) {}
+
+//	ngOnInit() {
+//		console.log('OnInit->EventManger');
+		//this.retts = this.eventService.getEventList();
+//	}
+	
+}
