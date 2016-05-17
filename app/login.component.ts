ac70d963e820f8e3ee207a8168121c8ed2603ed0
Federer <jfederer@usgs.gov>
2016-05-17 14:47:05 -0500

    All cleaned up

---
+++ app/login.component.ts
@@ -0,0 +1,15 @@
+import { Component }       from 'angular2/core';
+
+
+@Component ({
+	selector: 
+		'login-info',
+	templateUrl: 
+		'app/login.component.html',
+})
+
+
+
+export class LoginComponent {
+
+}
--- app/webff.component.ts
+++ app/webff.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from './app/login.component';
 
 @Component({
   selector: 'webff',
