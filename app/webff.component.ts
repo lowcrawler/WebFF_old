a1db9807c0605d523edcfcc9631ea011d97c9551
Federer <jfederer@usgs.gov>
2016-05-19 15:08:17 -0500

    Added view event that pulls from routesegment

--- app/webff.component.ts
+++ app/webff.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES, Routes } from '@angular/router';
 import { EventsManagerComponent } from './eventsManager.component';
 import { NewEventComponent } from './newEvent.component';
+import { ViewEditEventComponent } from './viewEditEvent.component';
 
 @Component({
   selector: 'webff',
@@ -14,7 +15,7 @@ import { NewEventComponent } from './newEvent.component';
   {path: '/events-manager',	component: EventsManagerComponent},
   {path: '/new-event',			component: NewEventComponent}, // TODO -- split out into types of new events?
  // {path: '*',				component: WebFieldForm},// TODO, need something for incorrect URLs ... this isn't good.  Technically still works, but console gives errors.
-  //{path: '/hero/:id',      component: HeroDetailComponent}
+  {path: '/view-edit-event/:eventid',      component: ViewEditEventComponent}
 ])
 
 
